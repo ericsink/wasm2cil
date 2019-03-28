8be95734a8961d7ff68d5e6d155279cdd29f3171
Eric Sink <eric@sourcegear.com>
2019-03-28 10:16:08 -0500

    add stopwatch to t Main

--- t/Program.fs
+++ t/Program.fs
@@ -1,14 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open System.IO
 
 open wasm.buffer
 open wasm.m
 
 [<EntryPoint>]
 let main argv =
-    let br = BinaryWasmStream(File.ReadAllBytes(argv.[0]))
+    let br = BinaryWasmStream(System.IO.File.ReadAllBytes(argv.[0]))
+    let timer = System.Diagnostics.Stopwatch.StartNew()
     let m = read_module br
+    timer.Stop()
+    printfn "%A milliseconds" timer.ElapsedMilliseconds
     printfn "%A" m
     0 // return an integer exit code
