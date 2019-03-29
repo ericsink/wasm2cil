b68683b23d42efdf67ede4c8d893e56163e4bf93
Eric Sink <eric@sourcegear.com>
2019-03-29 12:16:11 -0500

    tighten ReadBasic even more, with class methods now used only where needed

--- wasm/ReadBasic.fs
+++ wasm/ReadBasic.fs
@@ -5,50 +5,10 @@ module wasm.read_basic
         let buf = ba
         let mutable offset = 0
 
-        let read_byte () =
-            let b = buf.[offset]
-            offset <- offset + 1
-            b
-
-        let rec read_var_signed count (value: int64) =
-            let b = read_byte ()
-            let result = value ||| (int64 (b &&& 0x7Fuy) <<< (count * 7))
-            if (b &&& 0x80uy) = 0uy then
-                let sign = -1L <<< ((count + 1) * 7)
-                if ((sign >>> 1) &&& result) <> 0L then
-                    result ||| sign
-                else
-                    result
-            else
-                read_var_signed (count + 1) result
-
-        let rec read_var_unsigned count (value: uint32) =
-            let b = read_byte ()
-            let result = value ||| (uint32 (b &&& 0x7Fuy) <<< (count * 7))
-            if (b &&& 0x80uy) = 0uy then
-                result
-            else
-                read_var_unsigned (count + 1) result
-
         member this.ReadByte() =
             let b = buf.[offset]
             offset <- offset + 1
             b
-
-        member this.Remaining() =
-            buf.Length - offset
-
-        member this.Length() =
-            buf.Length
-
-        member this.ReadVarUInt32() =
-            read_var_unsigned 0 0u
-
-        member this.ReadVarInt32() =
-            read_var_signed 0 0L |> int32
-
-        member this.ReadVarInt64() =
-            read_var_signed 0 0L
 
         member this.ReadBytes(len: uint32) =
             let last = offset + (int len) - 1
@@ -56,28 +16,54 @@ module wasm.read_basic
             offset <- offset + int len
             ba
 
+        member this.Offset() =
+            offset
+
+        member this.Length() =
+            buf.Length
+
     let read_byte (br: BinaryWasmStream) =
         br.ReadByte()
 
     let read_bytes (br: BinaryWasmStream) (len :uint32) =
         br.ReadBytes(len)
 
-    let read_var_u32 (br: BinaryWasmStream) =
-        br.ReadVarUInt32()
+    let rec private read_var_signed br count (value: int64) =
+        let b = read_byte br
+        let result = value ||| (int64 (b &&& 0x7Fuy) <<< (count * 7))
+        if (b &&& 0x80uy) = 0uy then
+            let sign = -1L <<< ((count + 1) * 7)
+            if ((sign >>> 1) &&& result) <> 0L then
+                result ||| sign
+            else
+                result
+        else
+            read_var_signed br (count + 1) result
 
-    let read_var_i32 (br: BinaryWasmStream) =
-        br.ReadVarInt32()
+    let rec private read_var_unsigned br count (value: uint32) =
+        let b = read_byte br
+        let result = value ||| (uint32 (b &&& 0x7Fuy) <<< (count * 7))
+        if (b &&& 0x80uy) = 0uy then
+            result
+        else
+            read_var_unsigned br (count + 1) result
 
-    let read_var_i64 (br: BinaryWasmStream) =
-        br.ReadVarInt64()
+    let read_var_u32 br =
+        read_var_unsigned br 0 0u
 
-    let read_f32 (br: BinaryWasmStream) =
+    let read_var_i32 br =
+        read_var_signed br 0 0L |> int32
+
+    let read_var_i64 br =
+        read_var_signed br 0 0L
+
+    let read_f32 br =
         let ba = read_bytes br 4u
         use ms = new System.IO.MemoryStream(ba)
         use br = new System.IO.BinaryReader(ms)
         br.ReadSingle()
 
-    let read_f64 (br: BinaryWasmStream) =
+    let read_f64 br =
         let ba = read_bytes br 8u
         use ms = new System.IO.MemoryStream(ba)
         use br = new System.IO.BinaryReader(ms)
@@ -93,7 +79,7 @@ module wasm.read_basic
         v
 
     let remaining (br: BinaryWasmStream) =
-        br.Remaining()
+        br.Length() - br.Offset()
 
     let read_vector (br: BinaryWasmStream) count f =
         if count = 0u then 
