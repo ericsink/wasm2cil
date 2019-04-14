fb4982003b91e87ef39bc6ea7a14db49797b3394
Eric Sink <eric@sourcegear.com>
2019-04-14 16:00:53 -0500

    fix to_wasm.sh broken in prev commit

--- miniray/to_wasm.sh
+++ miniray/to_wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-clang --target=wasm32 -O3 foo.c -ffast-math -nostdlib -Wl,--no-entry -Wl,--export=miniray -Wl,--allow-undefined  -o miniray.wasm
+clang --target=wasm32 -O3 miniray.c -ffast-math -nostdlib -Wl,--no-entry -Wl,--export=miniray -Wl,--allow-undefined  -o miniray.wasm
 ../../wabt/bin/wasm2wat miniray.wasm > miniray.wat
 
