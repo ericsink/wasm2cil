7011935a3d38ec3ddcb0ba6f4ba1cd160b1172a0
Eric Sink <eric@sourcegear.com>
2019-04-17 14:11:47 -0500

    rm dead code from sqlite3/reference/run.c

--- sqlite3/reference/run.c
+++ sqlite3/reference/run.c
@@ -1,36 +1,9 @@
 
 #include <stdio.h>
-#include <sys/time.h>
-
-long get_ms(void)
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    long s = (long) (tv.tv_sec);
-    long us = (long) (tv.tv_usec);
-    long ms = us / 1000;
-    long total = s * 1000 + ms;
-    return total;
-}
-
-void checkpoint(int n)
-{
-    fprintf(stderr, "checkpoint: %d\n", n);
-}
-
-void dumpf(int n, float f)
-{
-    fprintf(stderr, "dumpf: %d -- %3.4f\n", n, f);
-}
-
-void dump_i32(int n, int v)
-{
-    fprintf(stderr, "dump_i32: %d -- %d\n", n, v);
-}
 
 int main()
 {
     int x = test2();
-    fprintf(stderr, "test1: %d\n", x);
+    fprintf(stderr, "result: %d\n", x);
 }
 
