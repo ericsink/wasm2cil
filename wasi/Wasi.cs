c48c9d36355760f480bc213e7abe245c4a3ef7bf
Eric Sink <eric@sourcegear.com>
2019-04-20 00:37:58 -0500

    significant progress on file io.  some basic cases work.  it turns out that wasi libc can't do much unless you give it a preopened directory with rights.

--- wasi/Wasi.cs
+++ wasi/Wasi.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 public class ProcExitException : Exception
 {
@@ -21,6 +22,9 @@ public static class util
             return null;
         }
 
+        // TODO not sure if we should be adding the 0 terminator or not
+        // TODO sqlite usually wants it I think
+
         byte[] byteArray;
         int nlen = Encoding.UTF8.GetByteCount(sourceText) + 1;
 
@@ -31,6 +35,7 @@ public static class util
         return byteArray;
     }
 
+#if not
     private static int GetNativeUTF8Size(System.IntPtr nativeString)
     {
         var offset = 0;
@@ -62,6 +67,7 @@ public static class util
 
         return result;
     }
+#endif
 
     public static string from_utf8(IntPtr nativeString, int size)
     {
@@ -83,31 +89,98 @@ public static class wasi_unstable
     public static int __mem_size;
     public static IntPtr __mem;
 
+    static Dictionary<int,FileStream> _files = new Dictionary<int,FileStream>();
+
+    static System.IO.Stream _stdin;
+    static System.IO.Stream _stdout;
+    static System.IO.Stream _stderr;
+    static Stream get_stream_for_fd(int fd)
+    {
+        switch (fd)
+        {
+            case 0:
+                if (_stdin == null)
+                {
+                    _stdin = System.Console.OpenStandardInput();
+                }
+                return _stdin;
+            case 1:
+                if (_stdout == null)
+                {
+                    _stdout = System.Console.OpenStandardOutput();
+                }
+                return _stdout;
+            case 2:
+                if (_stderr == null)
+                {
+                    _stderr = System.Console.OpenStandardError();
+                }
+                return _stderr;
+            default:
+                if (_files.TryGetValue(fd, out var strm))
+                {
+                    return strm;
+                }
+                else
+                {
+                    // TODO probably an error code
+                    throw new NotImplementedException();
+                }
+        }
+
+    }
+
+    // TODO fds are supposed to be more random
+    static int _nextFd = 10;
+
     public static int path_open(
 		int dirfd,
 		int dirflags,
-		int path,
+		int addr_path,
 		int path_len,
 		int oflags,
 		long fs_rights_base,
 		long fs_rights_inheriting,
 		int fs_flags,
-		int fd
+		int addr_fd
 		)
     {
-        throw new NotImplementedException();
+        // TODO very simplistic implementation
+        //System.Console.WriteLine("dirfd: {0}", dirfd);
+        var path = util.from_utf8(__mem + addr_path, path_len);
+        //System.Console.WriteLine("path: {0}", path);
+        var strm = File.Open(path, FileMode.OpenOrCreate);
+        var fd = _nextFd++;
+        _files[fd] = strm;
+        Marshal.WriteInt32(__mem + addr_fd, fd);
+        return 0;
     }
 
     public static int fd_prestat_dir_name(int fd, int addr_path, int len)
     {
-        throw new NotImplementedException(string.Format("fd {0}", fd));
+        switch (fd)
+        {
+            case 3:
+                Marshal.WriteByte(__mem + addr_path, 46); // .
+                //Marshal.WriteByte(__mem + addr_path + 1, 0);
+                return 0;
+            default:
+                throw new NotImplementedException(string.Format("fd {0}  len {1}", fd, len));
+        }
     }
     public static int fd_prestat_get(int fd, int addr)
     {
         // there is a loop that tries preopened file descriptors
         // starting at 3 until it finds a bad one.
-        return 8; // EBADF
-        //throw new NotImplementedException(string.Format("fd {0}", fd));
+        switch (fd)
+        {
+            case 3:
+                Marshal.WriteByte(__mem + addr, 0); // preopentype_dir
+                Marshal.WriteInt32(__mem + addr + 4, 1);
+                return 0;
+            default:
+                return 8; // EBADF
+        }
     }
     public static int environ_sizes_get(int addr_environ_count, int addr_environ_buf_size)
     {
@@ -154,77 +227,158 @@ public static class wasi_unstable
             default: throw new NotImplementedException();
         }
     }
-    public static int fd_close(int a)
+    public static int fd_close(int fd)
     {
-        throw new NotImplementedException();
+        if (_files.TryGetValue(fd, out var strm))
+        {
+            strm.Close();
+            _files.Remove(fd);
+            return 0;
+        }
+        else
+        {
+            // TODO err code
+            throw new NotImplementedException();
+        }
     }
     public static int fd_sync(int a)
     {
         throw new NotImplementedException();
     }
-    public static int fd_seek(int a, long b, int c, int d)
+    public static int fd_seek(int fd, long offset, int whence, int addr_newoffset)
     {
-        throw new NotImplementedException();
+        System.Console.WriteLine("fd_seek: fd {0} offset {1} whence {2}", fd, offset, whence);
+        var strm = get_stream_for_fd(fd);
+        SeekOrigin origin;
+        switch (whence)
+        {
+            case 0: // cur
+                origin = SeekOrigin.Current;
+                break;
+            case 1: // end
+                origin = SeekOrigin.End;
+                break;
+            case 2: // set
+                origin = SeekOrigin.Begin;
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+        strm.Seek(offset, origin);
+        return 0;
     }
-    public static int fd_read(int a, int b, int c, int d)
+    public static int fd_read(int fd, int addr_iovecs, int iovecs_len, int addr_nread)
     {
-        throw new NotImplementedException();
+        var a_iovecs = new int[iovecs_len * 2];
+        Marshal.Copy(__mem + addr_iovecs, a_iovecs, 0, iovecs_len * 2);
+
+        var strm = get_stream_for_fd(fd);
+
+        int total_len = 0;
+        for (int i=0; i<iovecs_len; i++)
+        {
+            var addr = a_iovecs[i * 2];
+            var len = a_iovecs[i * 2 + 1];
+            var ba = new byte[len];
+            // TODO ReadFully
+            strm.Read(ba, 0, len);
+            Marshal.Copy(__mem + addr, ba, 0, len);
+            total_len += len;
+        }
+
+        Marshal.WriteInt32(__mem + addr_nread, total_len);
+
+        return 0;
     }
     public static int poll_oneoff(int a, int b, int c, int d)
     {
         throw new NotImplementedException();
     }
-    static System.IO.Stream _stdout;
-    static System.IO.Stream _stderr;
-    public static int fd_write(int fd, int p_a_iovecs, int num_iovecs, int p_written)
+    public static int fd_write(int fd, int addr_iovecs, int iovecs_len, int addr_nwritten)
     {
-        //System.Console.WriteLine("fd_write: {0} {1} {2} {3}", fd, p_a_iovecs, num_iovecs, p_written);
-        System.IO.Stream strm;
-        switch (fd)
-        {
-            case 1:
-                if (_stdout == null)
-                {
-                    _stdout = System.Console.OpenStandardOutput();
-                }
-                strm = _stdout;
-                break;
-            case 2:
-                if (_stderr == null)
-                {
-                    _stderr = System.Console.OpenStandardError();
-                }
-                strm = _stderr;
-                break;
-            default:
-                throw new NotImplementedException();
-        }
+        //System.Console.WriteLine("fd_write: {0} {1} {2} {3}", fd, addr_iovecs, iovecs_len, addr_nwritten);
 
-        var a_iovecs = new int[num_iovecs * 2];
-        Marshal.Copy(__mem + p_a_iovecs, a_iovecs, 0, num_iovecs * 2);
+        var a_iovecs = new int[iovecs_len * 2];
+        Marshal.Copy(__mem + addr_iovecs, a_iovecs, 0, iovecs_len * 2);
+
+        var strm = get_stream_for_fd(fd);
 
         int total_len = 0;
-        for (int i=0; i<num_iovecs; i++)
+        for (int i=0; i<iovecs_len; i++)
         {
             var addr = a_iovecs[i * 2];
             var len = a_iovecs[i * 2 + 1];
             var ba = new byte[len];
             Marshal.Copy(__mem + addr, ba, 0, len);
+            // TODO WriteFully
             strm.Write(ba, 0, len);
             total_len += len;
         }
 
-        var ia = new int[] { total_len };
-        Marshal.Copy(ia, 0, __mem + p_written, 1);
+        Marshal.WriteInt32(__mem + addr_nwritten, total_len);
 
         //System.Console.WriteLine("  done fd_write");
 
         return 0;
     }
-    public static int fd_fdstat_get(int a, int b)
+    static void add_all_rights(int addr)
     {
-        // TODO what is this supposed to do?
-        return 0;
+        // TODO temporary.  in each case, think about what rights
+        // should actually be given.
+        for (int i=0; i<8; i++)
+        {
+            Marshal.WriteByte(__mem + addr + i, 0xff);
+        }
+    }
+    public static int fd_fdstat_get(int fd, int addr)
+    {
+        //System.Console.WriteLine("fd_fdstat_get: {0}", fd);
+        switch (fd)
+        {
+            case 0: // stdin
+                Marshal.WriteByte(__mem + addr + 0, 3); // dir
+                // TODO appropriate flags for stdin
+                Marshal.WriteInt16(__mem + addr + 2, 0); // flags
+                add_all_rights(addr + 8); // TODO rights
+                add_all_rights(addr + 16); // TODO inherit
+                return 0;
+            case 1: // stdout
+                Marshal.WriteByte(__mem + addr + 0, 3); // dir
+                // TODO appropriate flags for stdout
+                Marshal.WriteInt16(__mem + addr + 2, 0); // flags
+                add_all_rights(addr + 8); // TODO rights
+                add_all_rights(addr + 16); // TODO inherit
+                return 0;
+            case 2: // stderr
+                Marshal.WriteByte(__mem + addr + 0, 3); // dir
+                // TODO appropriate flags for stderr
+                Marshal.WriteInt16(__mem + addr + 2, 0); // flags
+                add_all_rights(addr + 8); // TODO rights
+                add_all_rights(addr + 16); // TODO inherit
+                return 0;
+            case 3:
+                Marshal.WriteByte(__mem + addr + 0, 3); // dir
+                // TODO appropriate flags for the pre dir
+                Marshal.WriteInt16(__mem + addr + 2, 0); // flags
+                add_all_rights(addr + 8); // TODO rights
+                add_all_rights(addr + 16); // TODO inherit
+                return 0;
+            default:
+                if (_files.TryGetValue(fd, out var strm))
+                {
+                    Marshal.WriteByte(__mem + addr + 0, 3); // dir
+                    // TODO appropriate flags for this file
+                    Marshal.WriteInt16(__mem + addr + 2, 0); // flags
+                    add_all_rights(addr + 8); // TODO rights
+                    add_all_rights(addr + 16); // TODO inherit
+                    return 0;
+                }
+                else
+                {
+                    // TODO probably an error code
+                    throw new NotImplementedException();
+                }
+        }
     }
     public static int fd_fdstat_set_flags(int a, int b)
     {
