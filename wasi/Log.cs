05edc704afe85fbf073c368869fbc17471d08971
Eric Sink <eric@sourcegear.com>
2019-04-28 11:32:22 -0500

    break Wasi.cs up, moving stuff unrelated to WASI out into separate files

---
+++ wasi/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class __log
+{
+    public static void Trace(string s)
+    {
+        System.Console.WriteLine("{0}", s);
+    }
+
+    public static void Trace2(object v, string s)
+    {
+        if (v is null)
+        {
+            System.Console.WriteLine("{0} NULL", s);
+        }
+        else
+        {
+            System.Console.WriteLine("{0} : {1}", s, v);
+        }
+    }
+}
+
