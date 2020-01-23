3ac3f9cbee7edc9132950999c011afa1268d27c0
Eric Sink <eric@sourcegear.com>
2020-01-23 00:31:00 -0600

    chg tool/ray.sh to compile the module into a dll and then rundll on the result

--- tool/ray.sh
+++ tool/ray.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-time dotnet run -- run ../../rust-raytracer/target/wasm32-wasi/release/rust-raytracer.rustc.wasm
+dotnet run -- compile ../../rust-raytracer/target/wasm32-wasi/release/rust-raytracer.rustc.wasm raytracer.dll
+time dotnet run -- rundll raytracer.dll
+
