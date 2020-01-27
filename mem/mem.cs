529cd012fdb7e1dc8046dce37e02aa97141fabbc
Eric Sink <eric@sourcegear.com>
2020-01-27 08:10:45 -0600

    add sg_wasm.get_span()

--- mem/mem.cs
+++ mem/mem.cs
@@ -4,5 +4,13 @@ public static class sg_wasm
 {
     public static int __mem_size;
     public static IntPtr __mem;
+
+    public static Span<byte> get_span(int addr, int len)
+    {
+        unsafe
+        {
+            return new Span<byte>((sg_wasm.__mem + (int) addr).ToPointer(), len);
+        }
+    }
 }
 
